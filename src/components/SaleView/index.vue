ee48f0b3531683976bd2dcaa59a6fa21fbffe0fb
wangyongalive <jsonwang0102@163.com>
2021-05-05 12:54:14 +0800

    saleView

--- src/components/SaleView/index.vue
+++ src/components/SaleView/index.vue
@@ -1,13 +1,56 @@
 <template>
-  <div>sales view</div>
+  <div class="sales-view">
+    <el-card shadow="hover">
+      <!-- 具名插槽 -->
+      <template v-slot:header>
+        <div class="memu-wrapper">
+          <el-menu
+            mode="horizontal"
+            :default-active="activeIndex"
+            @select="onMenuSelect"
+          >
+            <el-menu-item index="1">销售额</el-menu-item>
+            <el-menu-item index="2">访问量</el-menu-item>
+          </el-menu>
+          <div class="menu-right">
+            <el-radio-group v-model="radioSelect" size="small">
+              <el-radio-button label="今日"></el-radio-button>
+              <el-radio-button label="本周"></el-radio-button>
+              <el-radio-button label="本月"></el-radio-button>
+              <el-radio-button label="今年"></el-radio-button>
+            </el-radio-group>
+          </div>
+        </div>
+      </template>
+
+      <template>
+        <div>22222</div>
+      </template>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      activeIndex: "1",
+      radioSelect: "今日",
+    };
+  },
+  methods: {
+    onMenuSelect(index) {
+      this.activeIndex = index;
+    },
+  },
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.sales-view {
+  margin-top: 20px;
+  .memu-wrapper {
+    display: flex;
+  }
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,22 @@
 // 按需引用
-import Vue from 'vue'
-import { Row, Card, Col } from 'element-ui'
+import Vue from "vue";
+import {
+  Row,
+  Card,
+  Col,
+  Menu,
+  MenuItem,
+  RadioGroup,
+  RadioButton,
+  DatePicker
+} from "element-ui";
 
 /*插件*/
-Vue.use(Row)
-Vue.use(Col)
-Vue.use(Card)
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Card);
+Vue.use(Menu);
+Vue.use(MenuItem);
+Vue.use(RadioGroup);
+Vue.use(RadioButton);
+Vue.use(DatePicker);
